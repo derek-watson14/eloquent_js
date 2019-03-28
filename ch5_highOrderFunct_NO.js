5943b698b2cac54ad8b1979bd12b6d7916496dd0
derek-watson14 <derek.watson92@gmail.com>
2019-03-28 14:33:32 +0100

    Create ch5_highOrderFunct_NO.js
    Select notes from text of chapter 5, High-Order functions.

---
+++ ch5_highOrderFunct_NO.js
@@ -0,0 +1,12 @@
+function characterCount(script) {
+  return script.ranges.reduce((count, [from, to]) => {
+    // console.log(`${count}, ${script.name}`);
+    return count + (to - from);
+  }, 0);
+}
+
+console.log(SCRIPTS.reduce((a, b) => {
+  console.log(a, b);
+  return characterCount(a) < characterCount(b) ? b : a;
+}));
+// → {name: "Han", …}
