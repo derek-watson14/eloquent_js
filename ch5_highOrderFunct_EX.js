26f2c3d37d02e3c97599e0d4f8bb283f97ca5472
derek-watson14 <derek.watson92@gmail.com>
2019-03-29 15:31:27 +0100

    Update ch5_highOrderFunct_EX.js
    last two exercises in progress

--- ch5_highOrderFunct_EX.js
+++ ch5_highOrderFunct_EX.js
@@ -17,3 +17,45 @@ loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
 // → 2
 // → 1
+
+// 3. Everything
+function everyLoop(array, test) {
+  for (let element of array) {
+    if (!test(element)) return false;
+  }
+  return true;
+}
+function every(array, test) {
+  // n represents each array element, if any one element returns false from test,
+  // the if statement evaluates to true, and the function returns false
+  // if NO element tests false (ie all are under 10), the else code is executed,
+  // and the function returns true
+  if (array.some(n => !test(n))) return false;
+  else return true;
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
+
+
+// 4. Dominant writing direction
+function dominantDirection(text) {
+  let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.name : "none";
+  }).filter(({name}) => name != "none");
+  
+  let dom = scripts.reduce((a, b) => a.count < b.count ? b : a);
+  let dir = SCRIPTS.filter(s => s.name === dom);
+  
+  return dir;
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
